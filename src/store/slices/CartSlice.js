ddf33454a56b8d55dc91122e09e33df8de78f52c
juliovianadev <juliovianadev@gmail.com>
2023-03-03 17:50:14 -0300

    addCart finalizado

--- src/store/slices/CartSlice.js
+++ src/store/slices/CartSlice.js
@@ -2,9 +2,33 @@ import {createSlice} from '@reduxjs/toolkit'
 
 const CartSlice = createSlice({
   name: 'cart',
-  initialState: {},
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0
+  },
   reducers:{
+    addItemsToCart(state,action){
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      if(!existingItem){
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      }else{
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        
 
+      }
+    },
+    removeItemFromCart(){
+
+    }
   }
 })
 
