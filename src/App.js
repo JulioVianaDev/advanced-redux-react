a48a1b84f9a88df3090e651cbaa60365587e6d05
juliovianadev <juliovianadev@gmail.com>
2023-03-08 10:26:18 -0300

    a maneira correta de se fazer um useEffect com reducer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,13 @@ import { useSelector } from 'react-redux';
 function App() {
 
   const cartShow = useSelector(state=> state.ui.cartIsVisible);
+  const cart = useSelector(state=> state.cart);
+  useEffect(()=>{
+    fetch('https://advanced-redux-d764f-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  },[cart])
   return (
     <Layout>
      {cartShow &&  <Cart />}
